d23902a9765be8b85c359d5e17ac24b13226b472
GitHub Action <action@github.com>
2025-09-07 00:31:56 +0000

    Update universal-api bundles

--- universal-api.umd.js
+++ universal-api.umd.js
@@ -1,7 +1,7 @@
 /**
  * @caxperts/universal.api - UMD Bundle
  * Version: ** 7.2.1
- * Bundled on: 2025-08-31
+ * Bundled on: 2025-09-07
  * This file was automatically generated from the TypeScript source.
  */
 
