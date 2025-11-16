8c9c6b05863e5558479c12eae0ea881624b2f64a
GitHub Action <action@github.com>
2025-11-16 00:33:47 +0000

    Update universal-api bundles

--- universal-api.es.js
+++ universal-api.es.js
@@ -1,7 +1,7 @@
 /**
  * @caxperts/universal.api - ES Module Bundle
  * Version: ** 7.2.1
- * Bundled on: 2025-11-09
+ * Bundled on: 2025-11-16
  * This file was automatically generated from the TypeScript source.
  */
 
